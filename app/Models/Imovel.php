c728f0891f0f66834f8d0ddcd169c56dea4e977f
Daniel Lopes <danielferrari772@gmail.com>
2023-03-31 21:45:39 -0300

    Inserção das Models

---
+++ app/Models/Imovel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Imovel extends Model
+{
+    use HasFactory;
+	protected $table = 'imovel';
+	public $timestamps = false;
+}
