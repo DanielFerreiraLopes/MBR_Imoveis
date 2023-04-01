31b90ae02d1922225b61f5a5631ab4bcbdf23e36
Daniel Lopes <danielferrari772@gmail.com>
2023-03-31 22:24:38 -0300

    Teste de models

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Imagens;
+use App\Models\Imovel;
+use App\Models\Usuario;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function homeView()
     {
+        $imagens_imoveis = Imagens::all();
+        dd($imagens_imoveis);
         return view('home');
     }
 }
