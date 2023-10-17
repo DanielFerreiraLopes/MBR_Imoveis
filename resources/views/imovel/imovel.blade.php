3e19697893984d923880cb4740adaebaed7efcc4
DanielFerreiraLopes <danielferrari772@gmail.com>
2023-10-17 09:37:43 -0300

    style page venda

--- resources/views/imovel/imovel.blade.php
+++ resources/views/imovel/imovel.blade.php
@@ -1,96 +1,108 @@
-<title>Pesquisa - MBR Imoveis</title>
- <link rel="stylesheet" href="/css/imovel.css">
- <link rel="stylesheet" href="/css/flex.css">
-<x-layout>
+<title>Pesquisa - MBR Imoveis</title> <link rel="stylesheet" href="/css/imovel.css"> <link rel="stylesheet"
+    href="/css/flex.css">
+    <x-layout> <h1></h1>
 
-    <h1></h1>
-    
-    <!--<div id="box-info" class="flex-row">
+        <!--<div id="box-info" class="flex-row">
         <div class="flex-col content-center">
             <img id="img-main" src="{{ $imagens[0]->arquivo }}" alt="">
-        </div>
-        <div class="flex-col grow info-house">
-            <p>
+            </div>
+            <div class="flex-col grow info-house">
+                <p>
                 <span>{{ $imovel->cidade}} - {{ $imovel->estado}}</span> <br>
                 <span class="bairro">{{ $imovel->bairro}}</span> <br>
                 <span>{{ $imovel->rua}}, {{ $imovel->numero}}</span>
-            </p>
-            
-            <p>
+                </p>
+
+                <p>
                 <span>Quartos: {{ $imovel->quarto}}</span>
                 <span>Banheiros: {{ $imovel->banheiro}}</span>
                 <span>Preço: {{ $imovel->preco}}</span>
-            </p>
-            <p>
-                <span>{{ $imovel->descricao}}</span>
-            </p>
-        </div>
-        <div class="flex-col info-owner gap-30 content-center">
+                </p>
+                <p>
+                    <span>{{ $imovel->descricao}}</span>
+                    </p>
+            </div>
+            <div class="flex-col info-owner gap-30 content-center">
             <strong>Proprietário</strong>
             <span class="nome">{{ $proprietario->nome }}</span>
             <div class="flex-col gap-5">
-                <span class="email">Email: {{ $proprietario->email }}</span>
-                <span class="telefone">Telefone: {{ $proprietario->telefone }}</span>
+            <span class="email">Email: {{ $proprietario->email }}</span>
+            <span class="telefone">Telefone: {{ $proprietario->telefone }}</span>
             </div>
-        </div>
-    </div> -->
+            </div>
+        </div> -->
 
-    <div id="box-imovel">
-        <div id="section-img">
-            <img id="img-main" src="{{ $imagens[0]->arquivo }}" alt="">
-        </div>
+            <div id="box-imovel">
+                    <div id="section-img">
+                        <img id="img-main" src="{{ $imagens[0]->arquivo }}" alt="">
+                    </div>
 
-        <div id="section-imovel">
-            <p>
-                <span>{{ $imovel->cidade}} - {{ $imovel->estado}}</span> <br>
-                <span class="bairro">{{ $imovel->bairro}}</span> <br>
-                <span>{{ $imovel->rua}}, {{ $imovel->numero}}</span>
-            </p>
-            <br>
-             <p>
-                <span>Preço: R${{ $imovel->preco}}</span><br>
-                <span>Quartos: {{ $imovel->quarto}}</span><br>
-                <span>Banheiros: {{ $imovel->banheiro}}</span>  
-            </p>
-        </div>
-    </div>
+                <div id="section-imovel">
+                    <p>
+                        <span>{{ $imovel->cidade}} - {{ $imovel->estado}}</span> <br>
+                        <span class="bairro">{{ $imovel->bairro}}</span> <br>
+                        <span>{{ $imovel->rua}}, {{ $imovel->numero}}</span>
+                    </p>
+                    <br>
+                    <p>
+                        <span>Preço: R${{ $imovel->preco}}</span><br>
+                        <span>Quartos: {{ $imovel->quarto}}</span><br>
+                        <span>Banheiros: {{ $imovel->banheiro}}</span>
+                    </p>
+                </div>
+            </div>
 
-    <div class="box-imagens">
-        @foreach($imagens as $imagem)
-            <img src="{{ $imagem->arquivo }}" alt="" id="showimg">
-            <div id="myModal" class="modal">
+            <div id="box-proprietario"> 
+                <div id="section-proprietario"> 
+                    <strong>Proprietário</strong>
+                    <hr>
+                    <span class="nome">{{ $proprietario->nome }}</span><br>
+                    <span class="email">Email: {{ $proprietario->email }}</span><br>
+                    <span class="telefone">Telefone: {{ $proprietario->telefone }}</span>
+                </div>
+
+                <div id="section-descricao">
+                    <span>{{ $imovel->descricao}}</span>
+                </div>
+            </div>
+
+
+            <div class="box-imagens">
+                @foreach($imagens as $imagem)
+                <img src="{{ $imagem->arquivo }}" alt="" id="showimg">
+                <div id="myModal" class="modal">
                 <span class="close">&times;</span>
                 <img class="modal-content" id="img01">
                 <div id="caption"></div>
             </div>
-        @endforeach
-    </div>
-    
-    <div class="flex-row content-center space-30">
-        <a href="/">Voltar</a>
-    </div>
+            @endforeach
+        </div>
 
-    <script>
-        // Get the modal
-        var modal = document.getElementById("myModal");
+        <div class="flex-row content-center space-30">
+            <a href="/">Voltar</a>
+        </div>
 
-        // Get the image and insert it inside the modal - use its "alt" text as a caption
-        var img = document.getElementById("showimg");
-        var modalImg = document.getElementById("img01");
-        var captionText = document.getElementById("caption");
-        img.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-        }
+        <script>
+            // Get the modal
+            var modal = document.getElementById("myModal");
 
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
+            // Get the image and insert it inside the modal - use its "alt" text as a caption
+            var img = document.getElementById("showimg");
+            var modalImg = document.getElementById("img01");
+            var captionText = document.getElementById("caption");
+            img.onclick = function () {
+                modal.style.display = "block";
+                modalImg.src = this.src;
+                captionText.innerHTML = this.alt;
+            }
 
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() { 
-        modal.style.display = "none";
-        }
-    </script>
+            // Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close")[0];
 
-</x-layout>
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function () {
+                modal.style.display = "none";
+            }
+        </script>
+
+        </x-layout>
--- resources/views/imovel/venda.blade.php
+++ resources/views/imovel/venda.blade.php
@@ -9,21 +9,35 @@ $logado = Session::get("info_usuario");
 
     <link rel="stylesheet" href="/css/venda.css">
 
-<h1>Registre seu Imovel</h1>
+<h1>Registre seu Imóvel</h1>
+<hr>
 
     
         <form action="fazer-imovel" method="post" class='form-venda'>
-             <h4>Coloque as Informações sobre o imovel</h4>
+             <h4 style="text-align: center;">Coloque as Informações sobre o imovel</h4>
+             <p>
             <input type="number" name="quarto" placeholder="Quarto" value="{{ $logado->quarto }}" step="1">
             <input type="number" name="banheiro" placeholder="Banheiro" value="{{ $logado->banheiro }}" step="1">
             <input type="number" name="preco" placeholder="Preço" value="{{ $logado->preco }}" step="0.01">
+            </p>
+
+            <p>
             <input type="text" name="estado" placeholder="Estado" value="{{ $logado->estado }}">
             <input type="text" name="cidade" placeholder="Cidade" value="{{ $logado->cidade }}">
             <input type="text" name="rua" placeholder="Rua" value="{{ $logado->rua }}">
+            </p>
+
+            <p>
             <input type="text" name="bairro" placeholder="Bairro" value="{{ $logado->bairro }}">
             <input type="text" name="numero" placeholder="Numero" value="{{ $logado->numero }}">
             <input type="text" name="cep" placeholder="Cep" value="{{ $logado->cep }}">
-            <input type="text" name="descricao" placeholder="Descrição" value="{{ $logado->descrição }}"><br>
+            </p>
+
+            <p>
+            <textarea name="descricao" id="descricao" cols="30" rows="10" placeholder="Descrição..." value="{{ $logado->descrição }}"></textarea>
+                  <!--<input type="text" name="descricao" placeholder="Descrição" value="{{ $logado->descrição }}"><br> -->
+            </p>
+            <br>
             <input type="submit" value="Registrar" id="reg">
             @csrf
         </form>
